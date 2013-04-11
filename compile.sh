d9719d6e88dcc91f53f9c8247e1ae3be478c01d5
Arron Green <arrong@gmail.com>
2013-04-10 20:45:43 -0400

    removed compile shell script since it now uses gradle

--- compile.sh
+++
@@ -1,4 +0,0 @@
-rm -rf classes/
-rm -f MarketRatings.jar
-mkdir -p classes/
-javac -classpath /opt/hadoop-1.1.1/hadoop-core-1.1.1.jar:commons-cli-1.2.jar -d classes MarketRatings.java && jar -cvf MarketRatings.jar -C classes/ .
